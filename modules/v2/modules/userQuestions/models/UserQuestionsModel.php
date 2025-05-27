70f9049a6cd1922923671a9db04ce39b0692b1ac
Сергей Ткаченко <sitertm@yandex.ru>
2025-05-27 11:26:29 +0000

    DITVETAS-74  DITVETAS-76  UserQuestionsModel

---
+++ modules/v2/modules/userQuestions/models/UserQuestionsModel.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace app\modules\v2\modules\userQuestions\models;
+
+use app\models\db\UserQuestions;
+use app\models\db\Users;
+use app\modules\v2\modules\userQuestions\skeletons\QuestionsList;
+use yii\web\BadRequestHttpException;
+//use yii\web\ForbiddenHttpException;
+
+class UserQuestionsModel
+{
+    public $userQuestionsTable;
+    private $usersTable;
+
+    /**
+     * UserQuestionsModel constructor
+     */
+    public function __construct()
+    {
+        $this->userQuestionsTable = UserQuestions::tableName();
+        $this->usersTable = Users::tableName();
+    }
+
+    /**
+     * Возвращает выбранный вопрос
+     * 
+     * @param int $id
+     * @return UserQuestions
+     * @throws BadRequestHttpException
+     */
+    public function get(int $id)
+    {
+        $UserQuestions = UserQuestions::findOne(['id' => $id]);
+
+        if (empty($UserQuestions)) {
+            throw new BadRequestHttpException('Указанный вопрос не найден');
+        }
+
+        return $UserQuestions;
+    }
+
+    /**
+     * Постранично возвращает вопросы по фильтру
+     * 
+     * @param array $filter = []
+     * @param int $page = 1
+     * @param int $limit = 10
+     * @param string $type = 'all',
+     * @param string $attribute_name = 'userQuestions'
+     * @return CommonList
+     */
+    public function list(
+        array $filter = [],
+        int $page = 1,
+        int $limit = 10,
+        string $type = 'all',
+        string $attribute_name = 'userQuestions'
+    )
+    {
+        $query = UserQuestions::find()
+            ->select([
+                "{$this->userQuestionsTable}.id",
+                "TO_CHAR({$this->userQuestionsTable}.create_date, 'dd.mm.yyyy') AS create_date",
+                "TO_CHAR({$this->userQuestionsTable}.create_date, 'dd.mm.yy') AS create_date_short",  
+                "{$this->usersTable}.login",
+                "{$this->usersTable}.f_fio",
+                "{$this->usersTable}.i_fio",
+                "{$this->usersTable}.o_fio",
+                "{$this->userQuestionsTable}.question",
+                "{$this->userQuestionsTable}.answer",
+                "{$this->userQuestionsTable}.keywords"
+            ])
+            ->joinWith('questionUser', false);
+
+        if (isset($filter['status'])) {
+            if ($filter['status'] == 'has_answer') {
+                $query->andWhere(['NOT', ['answer' => null]]);
+            } elseif ($filter['status'] == 'wait_answer') {
+                $query->andWhere(['answer' => null]);
+            }
+        }
+        if (isset($filter['question_type'])) {
+            $query->andWhere(["{$this->userQuestionsTable}.question_type" => $filter['question_type']]);
+        }
+        if (isset($filter['question_user_id'])) {
+            $query->andWhere(["{$this->userQuestionsTable}.question_user_id" => $filter['question_user_id']]);
+        }
+        if (isset($filter['date_create'])) {
+            $query->andWhere(["TO_CHAR({$this->userQuestionsTable}.create_date, 'dd.mm.yyyy')" => $filter['date_create']]);
+        }
+        if (isset($filter['user_login'])) {
+            $query->andWhere(["{$this->usersTable}.login" => $filter['user_login']]);
+        }
+        if (isset($filter['answer_status'])) {
+            $query->andWhere(["{$this->userQuestionsTable}.answer_status" => $filter['answer_status']]);
+        }
+        if (isset($filter['search_status'])) {
+            $query->andWhere(["{$this->userQuestionsTable}.search_status" => $filter['search_status']]);
+        }
+        if (!empty($filter['keywords']) && is_array($filter['keywords'])) {
+            if (isset($filter['keywords_or'])) {
+                $where = ['OR'];
+                foreach ($filter['keywords'] as $keyword) {
+                    $where[] = ['ILIKE', "{$this->userQuestionsTable}.keywords", $keyword];
+                }
+                $query->andFilterWhere($where);
+            } else {
+                $query->andWhere(['ILIKE', "{$this->userQuestionsTable}.keywords", $filter['keywords']]);
+            }
+        }
+
+        $queryCount = clone $query;
+
+        $query->orderBY( "{$this->userQuestionsTable}.create_date")->limit($limit)->offset($limit * ($page - 1));
+		
+        return new QuestionsList(
+            $type,
+            $attribute_name,
+            $query->asArray()->all(),
+            $queryCount->count(),
+            $page,
+            $limit
+        );
+    }
+
+    /**
+     * Создает вопрос
+     *
+     * @param string $question_type
+     * @param string $question
+     * @param ?string $answer
+     * @param ?array $keywords
+     * @param ?string $real_file_name
+     * @param ?string $intr_file_name 
+     * @return UserQuestions
+     * @throws BadRequestHttpException
+     */
+    public function create(
+        string $question_type,
+        string $question,
+        ?string $answer = null,
+        ?array $keywords = null,
+        ?string $real_file_name = null,
+        ?string $intr_file_name = null)
+    {
+        $userQuestion = new UserQuestions();
+
+        $id_user = \Yii::$app->user->getId();
+        $userQuestion->question_user_id = $id_user;
+        $userQuestion->question_type = $question_type;
+        $userQuestion->question = $question;
+        if ($answer) {
+            $userQuestion->answer = $answer;
+            $userQuestion->answer_user_id = $id_user;
+            $userQuestion->answer_date = date('Y-M-d H:i:s');
+            $userQuestion->answer_status = 1;
+        } else {
+            $userQuestion->answer_status = 0;
+        }
+        $userQuestion->search_status = 1;
+        $userQuestion->keywords = $keywords ? implode(';', $keywords) : null;
+        $userQuestion->real_file_name = $real_file_name;
+        $userQuestion->intr_file_name = $intr_file_name;
+
+        if (!$userQuestion->save()) {
+            $errors = $userQuestion->getErrorSummary(true);
+            throw new BadRequestHttpException(empty($errors) ? 'Ошибка при создании справки' : implode("\n", array_values($errors)));
+        }
+
+        return $userQuestion;
+    }
+
+    /**
+     * Редактирует вопрос
+     *
+     * @param int $id
+     * @param ?string $question
+     * @param ?string $answer
+     * @param ?array $keywords
+     * @return UserQuestions
+     * @throws BadRequestHttpException
+     */
+    public function edit(
+        int $id,
+        ?string $question = null,
+        ?string $answer = null,
+        ?array $keywords = null)
+    {
+        if (!$question && !$answer && !$keywords) {
+            throw new BadRequestHttpException('Не передан ни один из параметров: question, answer или keywords');
+        }
+
+        $userQuestion = $this->get($id);
+
+        if ($question) {
+            $userQuestion->question = $question;
+        }
+        if ($answer) {
+            if (!$userQuestion->answer || $userQuestion->answer != $answer) {
+                $userQuestion->answer_user_id = \Yii::$app->user->getId();
+                $userQuestion->answer_date = date('Y-M-d H:i:s');
+            }
+            $userQuestion->answer = $answer;
+            $userQuestion->answer_status = 1;
+        }
+        if ($keywords) {
+            $userQuestion->keywords = implode(';', $keywords);
+        }
+
+        if (!$userQuestion->save()) {
+            $errors = $userQuestion->getErrorSummary(true);
+            throw new BadRequestHttpException(empty($errors) ? 'Ошибка при редактировании справки' : implode("\n", array_values($errors)));
+        }
+
+        return $userQuestion;
+    }
+}
